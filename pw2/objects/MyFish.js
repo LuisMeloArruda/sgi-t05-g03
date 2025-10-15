38677d0e319afceac0743103dcfabef64987f3cc
João Ferreira <up202208393@up.pt>
2025-10-15 08:02:55 +0100

    [ADD] Tentative of better fish geometry
    BUG: Geometry badly constructed; only 2 lines are visible in wireframe mode.

--- pw2/objects/MyFish.js
+++ pw2/objects/MyFish.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 
 // TODO: This is only a placeholder
-class MyFish extends THREE.Object3D {
+class MyBasicFish extends THREE.Object3D {
 
     constructor(
         // TODO
@@ -22,4 +22,79 @@ class MyFish extends THREE.Object3D {
     }
 }
 
-export { MyFish };
+class MyFish extends THREE.Object3D {
+    constructor(
+        width = 1,
+        head_width = 0.2,
+        height = 0.4,
+        depth = 0.3,
+        material = new THREE.MeshBasicMaterial({color: 0x1e66f5, side: THREE.DoubleSide}) // TODO
+    ) {
+        super()
+        this.width = width;
+        this.head_width = head_width;
+        this.height = height;
+        this.depth = depth;
+        this.material = material;
+        this.base_vertices = 3;
+        this.build();
+    }
+    
+    build() {
+        const body = this.build_body();
+        const upper_fin = this.build_upper_fin();
+        const bottom_left_fin = this.build_bottom_left_fin();
+        const bottom_right_fin = this.build_bottom_right_fin();
+        
+        // TODO: Transformations
+        
+        this.add(body);
+    }
+    
+    build_body() {
+        // BUG: Geometry badly constructed; only 2 lines are visible in wireframe mode.
+        
+        let vertices = [
+            -this.width / 2, 0, 0,
+            this.width / 2, 0, 0
+        ];
+        
+        const angle_step = Math.PI * 2 / this.base_vertices;
+        const base_x = (-this.width / 2) + this.head_width;
+        for (let angle = 0; angle < Math.PI * 2; angle += angle_step) {
+            const base_z = Math.cos(angle_step) * this.depth / 2;
+            const base_y = Math.sin(angle_step) * this.height / 2;
+            vertices.push(base_x, base_y, base_z);
+        }
+        console.log(vertices)
+        
+        let indices = [];
+        for (let face = 2; face < this.base_vertices + 2; face++) {
+            const left_end = 0;
+            const right_end = 1;
+            const next_face_vertice = ((face - 2 + 1) % this.base_vertices) + 2;
+            indices.push(face, next_face_vertice, left_end);
+            indices.push(right_end, next_face_vertice, face);
+        }
+        console.log(indices)
+        
+        let geometry = new THREE.BufferGeometry();
+        geometry.setIndex( indices );
+        geometry.setAttribute('position', new THREE.Float32BufferAttribute( vertices, 3 ))
+        const mesh = new THREE.Mesh(geometry, this.material);
+        return mesh;
+    }
+    
+    build_upper_fin() {
+        // TODO
+    }
+    
+    build_bottom_left_fin() {
+        // TODO
+    }
+    
+    build_bottom_right_fin() {
+        // TODO
+    }
+}
+export { MyBasicFish, MyFish };
