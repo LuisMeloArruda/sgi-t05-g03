4833f07648abb67b4f84b3e93ce7a62dca8e85f6
João Ferreira <up202208393@up.pt>
2025-10-19 11:57:24 +0100

    [ADD] Fish tail

--- pw2/objects/MyFish.js
+++ pw2/objects/MyFish.js
@@ -24,41 +24,45 @@ class MyBasicFish extends THREE.Object3D {
 
 class MyFish extends THREE.Object3D {
     constructor(
-        width = 1,
+        body_width = 1,
         head_width = 0.2,
+        tail_width = 0.1,
         height = 0.3,
         depth = 0.4,
         material = new THREE.MeshBasicMaterial({color: 0x1e66f5, side: THREE.DoubleSide}) // TODO
     ) {
         super()
-        this.width = width;
+        this.body_width = body_width;
         this.head_width = head_width;
+        this.tail_width = tail_width;
         this.height = height;
         this.depth = depth;
         this.material = material;
         this.base_vertices = 5;
+        this.tail_filling = 0.5;
         this.build();
     }
     
     build() {
         const body = this.build_body();
+        const tail = this.build_tail();
         const upper_fin = this.build_upper_fin();
         const bottom_left_fin = this.build_bottom_left_fin();
         const bottom_right_fin = this.build_bottom_right_fin();
         
         // TODO: Transformations
         
-        this.add(body);
+        this.add(body, tail);
     }
     
     build_body() {        
         let vertices = [
-            -this.width / 2, 0, 0,
-            this.width / 2, 0, 0
+            -this.body_width / 2, 0, 0,
+            this.body_width / 2, 0, 0
         ];
         
         const angle_step = Math.PI * 2 / this.base_vertices;
-        const base_x = (-this.width / 2) + this.head_width;
+        const base_x = (-this.body_width / 2) + this.head_width;
         for (let angle = 0; angle < Math.PI * 2; angle += angle_step) {
             const base_z = Math.cos(angle + (Math.PI / 2)) * this.depth / 2;
             const base_y = Math.sin(angle + (Math.PI / 2)) * this.height / 2;
@@ -81,8 +85,28 @@ class MyFish extends THREE.Object3D {
         return mesh;
     }
     
+    build_tail() {
+        let vertices = [
+            this.body_width / 2, 0, 0,
+            this.body_width / 2 + this.tail_width * this.tail_filling, 0, 0,
+            this.body_width / 2 + this.tail_width, this.height / 2, 0,
+            this.body_width / 2 + this.tail_width, -this.height / 2, 0
+        ];
+        
+        let indices = [
+            2, 0, 1,
+            1, 0, 3,
+        ];
+        
+        let geometry = new THREE.BufferGeometry();
+        geometry.setIndex( indices );
+        geometry.setAttribute('position', new THREE.Float32BufferAttribute( vertices, 3 ))
+        const mesh = new THREE.Mesh(geometry, this.material);
+        return mesh;
+    }
+    
     build_upper_fin() {
-        // TODO
+        
     }
     
     build_bottom_left_fin() {
